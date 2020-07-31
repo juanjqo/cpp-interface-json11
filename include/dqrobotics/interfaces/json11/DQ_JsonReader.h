174876913b01499c5f0d2f4b3ba50f06da5649c6
Murilo Marinho <murilo@nml.t.u-tokyo.ac.jp>
2020-07-31 16:33:34 +0900

    Fixed incorrect reader file placement. - Fixed license header on DQ_JsonReader.cpp

--- include/dqrobotics/interfaces/json11/DQ_JsonReader.h
+++ include/dqrobotics/interfaces/json11/DQ_JsonReader.h
@@ -1,29 +1,40 @@
 /**
 (C) Copyright 2019 DQ Robotics Developers
+
 This file is part of DQ Robotics.
+
     DQ Robotics is free software: you can redistribute it and/or modify
     it under the terms of the GNU Lesser General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
+
     DQ Robotics is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU Lesser General Public License for more details.
+
     You should have received a copy of the GNU Lesser General Public License
     along with DQ Robotics.  If not, see <http://www.gnu.org/licenses/>.
+
 Contributors:
-- Murilo M. Marinho (murilo@nml.t.u-tokyo.ac.jp)
+- Murilo M. Marinho        (murilo@nml.t.u-tokyo.ac.jp)
 */
 
 #include <dqrobotics/robot_modeling/DQ_SerialManipulator.h>
+#include <dqrobotics/robot_modeling/DQ_SerialManipulatorDH.h>
 
 namespace DQ_robotics
 {
 
 class DQ_JsonReader
 {
+private:
+    static DQ_SerialManipulator _get_serial_manipulator_from_json(const std::string& file);
+    static DQ_SerialManipulatorDH _get_serial_manipulator_dh_from_json(const std::string& file);
 public:
-    static DQ_SerialManipulator get_serial_manipulator_from_json(const std::string& file);
+    template <typename T>
+    static T get_from_json(const std::string& file);
 };
+
 
 }
